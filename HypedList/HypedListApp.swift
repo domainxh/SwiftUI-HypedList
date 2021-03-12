82fbd9200c8e57ab8a397ed60b13d524f46df0f4
Xiaoheng <domain.xh@gmail.com>
2021-03-12 10:38:41 -0500

    Initial commit

---
+++ HypedList/HypedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypedListApp.swift
+//  HypedList
+//
+//  Created by Xiaoheng Pan on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct HypedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HypedListTabView()
+        }
+    }
+}
---
+++ HypedList/HypedListTabView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HypedList
+//
+//  Created by Xiaoheng Pan on 3/12/21.
+//
+
+import SwiftUI
+
+struct HypedListTabView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct HypedListTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        HypedListTabView()
+    }
+}
